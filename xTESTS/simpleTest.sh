b0a26512098c57df8c9f0de2a5672f231ff93d22
Marek Salat <xsalat00@stud.fit.vutbr.cz>
2011-12-09 17:27:41 +0100

    xTESTS

---
+++ xTESTS/simpleTest.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+exitCode=$1;
+dir=$2
+
+
+ls -1 $dir | grep --invert-match ".sh$" | {  
+  while read file; do
+    pgr=`./project "$dir/$file"  2>&1`
+    err=$?
+    val=`valgrind --leak-check=full --leak-resolution=high -q ./project "$dir/$file"  2>&1`
+  
+    if [ $err -ne $exitCode -o "$pgr" != "$val" ]; then
+      if [ $err -eq exitCode ] ; then
+        valgrind --leak-check=full --leak-resolution=high -q ./project "$dir/$file"  
+      fi
+      echo "TEST FAIL - $file"  >&2;
+    else  
+      echo "TEST OK - $file";
+    fi
+  done
+}
