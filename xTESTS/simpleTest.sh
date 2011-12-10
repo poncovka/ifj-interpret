15440bfa88387987fce298ec16c106489f4f60b2
Marek Salat <xsalat00@stud.fit.vutbr.cz>
2011-12-10 13:54:43 +0100

    opraveny nektere testy v xTests

--- xTESTS/simpleTest.sh
+++ xTESTS/simpleTest.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 exitCode=$1;
 dir=$2
-
+cntOK=0;
+cntFail=0;
 
 ls -1 $dir | grep --invert-match "^README$" | {  
   while read file; do
@@ -10,6 +11,7 @@ ls -1 $dir | grep --invert-match "^README$" | {
     val=`valgrind --leak-check=full --leak-resolution=high -q ./project "$dir/$file"  2>&1`
   
     if [ "$err" != "$exitCode" -o "$pgr" != "$val" ]; then
+      cntFail=$cntFail+1
       printf "TEST FAIL - "  >&2;
       if [ "$pgr" != "$val" ] ; then
         printf "%s\n" $file >&2
@@ -19,6 +21,9 @@ ls -1 $dir | grep --invert-match "^README$" | {
       fi
     else  
       echo "TEST OK - $file";
+      cntOk=$cntOk+1
     fi
   done
 }
+cnt= $cntOk+$cntFail
+echo -e "OK\t[$cnt/$cntOk]\nFAIL\t[$cnt/$cntFail]" >&2
